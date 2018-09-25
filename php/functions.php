f455388acf8fbca8c5fa6e5757e2bbbac5986a58
alex van tilburg <alexvantilburg@outlook.com>
2018-09-25 22:30:36 +0200

    form added

--- php/functions.php
+++ php/functions.php
@@ -1,31 +1,50 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
 
-    $mysqli = new mysqli('127.0.0.1', 'root', '', 'facebible');
-    if ($mysqli->connect_error) {
-        die('Connect Error (' . $mysqli->connect_errno . ') '
-                . $mysqli->connect_error);
-    }
+        <form action="functions.php" method="post">
+            Username: <input type="text" name="name"><br>
+            Email: <input type="text" name="email"><br>
+            Password: <input type="text" name="password"><br>
+            <input type="submit" name="createAccount">
+        </form>
 
-    $password = 'ebboer';
-    $username = 'banaan';
-    $email = 'huis@gmail.com';
-    $hash = hash("sha256", $password);
+        <?php
 
-    accountMaken($mysqli, $username, $email, $hash);
+        $mysqli = new mysqli('127.0.0.1', 'root', '', 'facebible');
+        if ($mysqli->connect_error) {
+            die('Connect Error (' . $mysqli->connect_errno . ') '
+                    . $mysqli->connect_error);
+        }
 
-    $mysqli->close();
+        if ($_POST["createAccount"]) {
+            $password = $_POST["password"];
+            $username = $_POST["name"];
+            $email = $_POST["email"];
+            $hash = hash("sha256", $password);
 
-    function accountMaken($link, $username, $email, $hash) {
+            accountMaken($mysqli, $username, $email, $hash);
+        }
 
-        $query = "INSERT INTO accounts (username, email, hashedPassword)
-        VALUES ('$username', '$email', '$hash')";
-        $link->query($query);
-    }
+        $mysqli->close();
 
-    function accountOphalen($link, $username) {
+        function accountMaken($link, $username, $email, $hash) {
 
-        $query = "SELECT * FROM accounts
-        WHERE username='$username'";
-        $result = $link->query($query);
-    }
-?>
+            $query = "INSERT INTO accounts (username, email, hashedPassword)
+            VALUES ('$username', '$email', '$hash')";
+            $link->query($query);
+        }
+
+        function accountOphalen($link, $username) {
+
+            $query = "SELECT * FROM accounts
+            WHERE username='$username'";
+            $result = $link->query($query);
+        }
+        ?>
+
+    </body>
+</html>
+
